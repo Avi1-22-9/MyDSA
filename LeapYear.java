8bfca976e8b405036a54e15f1c8fdfbbbccb7bfd
MEDI AVINASH <mediavinash107@gmail.com>
2025-03-13 11:57:19 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Year : ");
+        int n =in.nextInt();
+        if(n%4==0 && n%100!=0 || n%400==0){
+            System.out.println("The Year" + n + " is a leap Year");
+
+        }
+        else{
+            System.out.println("The Year " + n + " is NOT a Leap Year!");
+        }
+    }
+}
